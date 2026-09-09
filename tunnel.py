f3de26484da13f3d72b6f8eea8660e3d6df5bd51
Joseph Saah <joesaah2136@gmail.com>
2026-09-09 17:59:12 -0400

    Allow user to pass in port as command line argument. Default to 8000

--- tunnel.py
+++ tunnel.py
@@ -1,6 +1,7 @@
 import subprocess
+import sys
 
-PORT = 8000
+PORT = sys.argv[1] if len(sys.argv) > 1 else 8000
 print(f"Starting tunnel for port {PORT}...")
 
 # Uses built-in SSH to route traffic through localhost.run
